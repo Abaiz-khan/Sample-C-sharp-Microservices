0992695f0ef013222d574ae2aded97afc1b6fc94
Abaiz <117434269+Abaiz-khan@users.noreply.github.com>
2024-02-10 01:12:10 +0500

    Update variables.tf

--- iac/variables.tf
+++ iac/variables.tf
@@ -1,5 +1,6 @@
-variable access_key {
+variable "access_key" {
   type        = string
   default     = "w4CNFfLecCf1pg5wmAnN9Kh6D+deP8jIxlHPyXM5JNlx2ji0suUEFaHyboIZaUlwfeiHVFyaQ90F+AStM1njRQ=="
   description = "access key for backend configuration"
 }
+
