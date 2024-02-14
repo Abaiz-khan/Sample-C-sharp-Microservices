eaa5addac50b8a04417aa0768dd7ea08642ae09a
Abaiz-khan <abaizkhn7@gmail.com>
2024-02-14 20:46:48 +0500

    applied unit test with nunit

---
+++ Sample-services/world-service/WorldService.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace WorldService.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
