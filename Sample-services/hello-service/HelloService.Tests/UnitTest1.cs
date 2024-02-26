cc60cde3495de1972803a5686abbcf7b46247b5c
Abaiz <117434269+Abaiz-khan@users.noreply.github.com>
2024-02-27 01:30:41 +0500

    Update UnitTest1.cs
    adjusting the unit test for hello

--- Sample-services/hello-service/HelloService.Tests/UnitTest1.cs
+++ Sample-services/hello-service/HelloService.Tests/UnitTest1.cs
@@ -1,7 +1,7 @@
 using HelloService.Controllers;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging.Abstractions; // Import NullLogger
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Abstractions; // Add this namespace
 using NUnit.Framework;
 
 namespace HelloService.Tests.Controllers
@@ -12,18 +12,23 @@ namespace HelloService.Tests.Controllers
         public void Get_ReturnsHello()
         {
             // Arrange
-            var logger = NullLogger<HelloController>.Instance;
-            var configuration = new ConfigurationBuilder().Build();
-            var controller = new HelloController(logger, configuration);
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "FeatureFlags:HelloFeatureEnabled", "true" } // Set the feature flag to true
+                })
+                .Build();
+
+            var controller = new HelloController(new NullLogger<HelloController>(), configuration);
 
             // Act
             IActionResult result = controller.Get();
 
             // Assert
-            Assert.IsNotNull(result);
+             Assert.IsNotNull(result);
             var okResult = result as OkObjectResult;
             Assert.IsNotNull(okResult);
-            Assert.AreEqual("Hello", okResult.Value); // Make sure to assert for "Hello" instead of hello
+            Assert.AreEqual("Hello", okResult.Value);
         }
     }
 }
